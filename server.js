e7d3c3ccab681b9e8521c0a0a56f20caa943ae30
dancastillo <dancastillo99@gmail.com>
2017-10-26 21:05:11 -0400

    server.js: added mongodb connection to learntoprogramtv db

--- server.js
+++ server.js
@@ -4,5 +4,18 @@ const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 
+mongoose.Promise = global.Promise;
+
+const options = {
+  useMongoClient: true
+};
+
+const uri = 'mongodb://localhost:27017/learntoprogramtv';
+
+mongoose.connect(uri, options).then(
+  () => { console.log("Connected to mongodb....") },
+  err => { console.log(err) }
+);
+
 app.listen(port); 
 console.log('App running on ' + port);
