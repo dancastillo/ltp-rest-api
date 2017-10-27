7319dc33210ddda20d3b30a0ce40f6776baf4090
dancastillo <dancastillo99@gmail.com>
2017-10-26 21:52:18 -0400

    server.js: added app.use and routes to app

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+// import node modules
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const List = require("./api/models/model");
 
 mongoose.Promise = global.Promise;
 
@@ -12,10 +14,20 @@ const options = {
 
 const uri = 'mongodb://localhost:27017/learntoprogramtv';
 
+// connect to mongodb
 mongoose.connect(uri, options).then(
   () => { console.log("Connected to mongodb....") },
   err => { console.log(err) }
 );
+
+// handle incoming requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const routes = require("./api/routes/routes");
+
+// register routes
+routes(app);
 
 app.listen(port); 
 console.log('App running on ' + port);
