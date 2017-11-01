10f30f5e1740a52fc4e2aee297d756faffa5339f
dancastillo <dancastillo99@gmail.com>
2017-11-01 10:02:29 -0400

    routes.js: added empty route and get all fields route

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,6 +3,9 @@ module.exports = (app) => {
   const ltpList = require('../controllers/controller');
 
   // our Routes
+  app.route('/')
+  .get(ltpList.get_list_items_fields);
+
   app.route('/lists')
     .get(ltpList.get_list_items) 
     .post(ltpList.create_list_item);
@@ -12,6 +15,6 @@ module.exports = (app) => {
     .put(ltpList.update_list_item)
     .delete(ltpList.delete_list_item);
 
-  app.route('/list/empty')
+  app.route('/empty-list')
     .delete(ltpList.delete_all_items);
 }
